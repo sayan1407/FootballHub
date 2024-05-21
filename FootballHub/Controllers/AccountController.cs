0caf7a8df2d3bbe689dfd7d96de8208397dafbc2
Sayan Saha <sayan199783@gmail.com>
2024-05-21 10:46:08 +0530

    Login and Register UI Added

---
+++ FootballHub/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FootballHub.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ FootballHub/Models/RegisterVm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootballHub.Models
+{
+    public class RegisterVM
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [Compare("Password")]
+        public string  ConfirmPassword { get; set; }
+    }
+}
